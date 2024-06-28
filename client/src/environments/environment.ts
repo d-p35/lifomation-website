6ab3f6f715301a4ca719d2bf03a5536b75f745a1
Yashank Bhola <yashankbhola@hotmail.com>
2024-06-28 17:20:26 -0400

    chore: :art:

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -5,6 +5,10 @@
 export const environment = {
   production: false,
   apiEndpoint: 'http://localhost:3000',
+  auth: {
+    domain: 'dev-8tnbwxvwy07fx47f.us.auth0.com',
+    clientId: 'v1i2B84ygCwXodkpfESsQnGZ9FTXF9LN'
+  }
 };
 
 /*
